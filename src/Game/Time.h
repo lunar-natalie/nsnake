32dc7e0f51b210c6a90e2c123dcdc878ba21a397
Natalie Wiggins <islifepeachy@outlook.com>
2023-12-25 02:23:50 +0000

    Add time types/literals

---
+++ src/Game/Time.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 Natalie Wiggins. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include <chrono>
+
+namespace nsnake {
+    namespace chrono = std::chrono;
+    using namespace std::chrono_literals;
+
+    using Clock = chrono::steady_clock;
+
+    constexpr static int TARGET_FRAMERATE = 24;
+
+    using FrameDuration = chrono::duration<double, std::ratio<1, TARGET_FRAMERATE>>;
+    constexpr static auto ONE_FRAME = FrameDuration(1s);
+}// namespace nsnake
