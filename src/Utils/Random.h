f24fe019cf25dd94e1a52b19007268c37926ee1d
Natalie Wiggins <islifepeachy@outlook.com>
2023-12-25 02:21:34 +0000

    Add RNG wrapper class

---
+++ src/Utils/Random.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Natalie Wiggins. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include <random>
+
+namespace nsnake {
+    class IntGenerator {
+        std::random_device m_rd;// Seed source
+        std::mt19937 m_gen;     // mersenne_twister_engine
+
+    public:
+        IntGenerator() : m_gen(m_rd()) {
+        }
+
+        int dist(int begin, int end) {
+            return std::uniform_int_distribution<>(begin, end)(m_gen);
+        }
+    };
+}// namespace nsnake
