b3d15a9410021c1b63fe5724f015916e87f443ac
Natalie Wiggins <islifepeachy@outlook.com>
2024-01-16 17:54:16 +0000

    Update version formatting

--- src/ProgramInfo.h
+++ src/ProgramInfo.h
@@ -10,5 +10,6 @@ namespace nsnake {
     const std::string VERSION_INFO = PROGRAM_NAME + " " NSNAKE_VERSION "\n"
                                                     "Copyright (c) 2024 Natalie Wiggins. All rights reserved.\n"
                                                     "Provided under the terms of the BSD 3-Clause license.\n"
-                                                    "You should have received a copy of the license along with this program. If not, see <https://opensource.org/license/bsd-3-clause/>.";
+                                                    "You should have received a copy of the license along with this program.\n"
+                                                    "If not, see <https://opensource.org/license/bsd-3-clause/>.";
 }// namespace nsnake
